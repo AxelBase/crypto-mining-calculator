ebb680c4c95d105ccb1480123e848601d28f463a
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-12-15 20:57:41 +0200

    GSC

---
+++ docs/_app/immutable/chunks/CHqp_CXk.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1fsjggt?.base??"/crypto-mining-calculator",t=globalThis.__sveltekit_1fsjggt?.assets??s??"";export{t as a,s as b};
--- docs/_app/immutable/chunks/DAI1gxH7.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1wqiyf0?.base??"/crypto-mining-calculator",a=globalThis.__sveltekit_1wqiyf0?.assets??s??"";export{a,s as b};
